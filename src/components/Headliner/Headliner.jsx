b2fd11d22698ae89781f9267feb4bbd6d7ac6f5c
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-12 21:40:22 -0400

    headliner component with basic ui added

---
+++ src/components/Headliner/Headliner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Headliner = () => {
+    return (
+        <section className="global-page-header">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="block">
+                        <h2>Headline</h2>
+                        <p>Quote/Subtitle</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Headliner;
