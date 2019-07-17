8c1066b5668a2a7653fac49ad6ef31d24fa13276
Rakesh Jain <rakeshvora2007@gmail.com>
2019-07-16 20:31:44 -0400

    Work Experience component added

---
+++ src/components/Pages/WorkEx/WorkEx.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const WorkEx = () => {
+    return (
+        <section class="company-description">
+        <div class="container">
+            <div class="education">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="block">
+                            <h3 class="subtitle wow fadeInUp animated" data-wow-delay=".3s" data-wow-duration="500ms">AutoXplorer, Irvine</h3>
+                            <h4 class="wow fadeInUp animated" data-wow-delay=".3s" data-wow-duration="500ms"><i><b>Software Engineer, Irvine, USA</b></i></h4>
+                            <p class="block wow fadeInUp animated" data-wow-duration="400ms" data-wow-delay="600ms">
+                                <ul>
+                                    <li>Developed a inventory management software using React, Redux and ​ Node.js. Collect dealer’s vehicle information and send
+                                            data to other supporting applications</li>
+                                    <li>Implemented the ​ Web Front End based​ on ​ JavaScript​ , ​ JQuery and HTML/CSS</li>
+                                    <li>Designed server to handle data from websites and stored it onto ​ SQL Server​ and passed to other utility websites asynchronously</li>
+                                    <li>Designed server that calls company’s different ​ web API​ to send user data</li>
+                                    <li>Developed ​ DevOps​ workflow using ​ Docker​ , ​ Git​ , ​ Github ​ and ​ Kubernetes</li>
+                                    <li>Created automated test scripts and integration on new code using ​ Travis CI</li>
+                                    <li>Followed ​ Agile​ methodology and Test Driven Development using ​ MOCHA and CHAI,​ created task on ​ Kanban​ ​ Board (Trello)</li>
+                                    <li>Used ​ Web Services ​ for communicating with other application and components</li>
+                                    <li>Performed maintenance and updation to existing client ​ Wordpress​ websites</li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 wow fadeInLeft" data-wow-delay=".3s">
+                            <img src="images/about/20958696.jpg" alt="" class="img-responsive"/>
+                        </div>
+                </div>
+                <br/>
+                <div class="row">
+                    <div class="col-md-6 wow fadeInLeft" data-wow-delay=".3s">
+                        <img src="images/about/prime.jpg" alt="" class="img-responsive"/>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="block">
+                            <h3 class="subtitle wow fadeInUp animated" data-wow-delay=".3s" data-wow-duration="500ms">Prime Software Solutions, Mumbai</h3>
+                            <h4 class="wow fadeInUp animated" data-wow-delay=".3s" data-wow-duration="500ms"><i><b>Software Developer, Mumbai, India</b></i></h4>
+                            <p class="block wow fadeInUp animated" data-wow-duration="400ms" data-wow-delay="600ms">
+                                <ul>
+                                    <li>Developed and maintained the front-end functionality of websites using HTML, CSS and
+                                        JavaScript.</li>
+                                    <li>Designed, implemented and supported database development on an SQL server.</li>
+                                    <li>Deployed multi-user applications within a service oriented architecture using restful
+                                        web services.</li>
+                                    <li>Built Android applications Prime Orders and Prime Gold using Android Studio.</li>
+                                    <li>Developed Servlets and back-end Java classes and deployed on Glassfish Application server.</li>
+                                    <li>Created complex SQL queries and used JDBC connectivity to access the database.</li>
+                                    <li>Resolved application bugs and connectivity problems in Java programs.</li>
+                                    <li>Maintained and updated existing Java GUI applications and modules using JBuilder, XML
+                                        and Swing.</li>
+                                    <li>Provided assistance and support to programming team members as required.</li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default WorkEx
