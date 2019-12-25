e2fadada4508382abe5c42555d980c9c87c5a39a
Rakesh Jain <rakeshvora2007@gmail.com>
2019-12-24 21:20:29 -0500

    header fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-   entry: './src/index.js',
-   output: {
-      path: path.join(__dirname, '/dist'),
-      filename: 'bundle.js'
-   },
-   devServer: {
-      inline: true,
-      port: 8080
-   },
-   module: {
-      rules: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-         },
-      ]
-   },
-   plugins:[
-      new HtmlWebpackPlugin({
-         template: __dirname + '/index.html',
-         filename: __dirname + '/dist/index.html',
-         inject: 'body'
-      })
-   ]
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/index.html",
+      filename: __dirname + "/dist/index.html",
+      inject: "body"
+    })
+  ]
+};
