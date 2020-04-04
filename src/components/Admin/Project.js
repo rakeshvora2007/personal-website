fa741e014eac08ad391a5319c29f1cb5ca1d3059
Rakesh Jain <rakeshvora2007@gmail.com>
2020-04-03 22:43:17 -0400

    image upload with custom storage solution

--- src/components/Admin/Project.js
+++ src/components/Admin/Project.js
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export const Project = () => {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState();
+  const [projectName, setProjectName] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [technologies, setTechnologies] = useState([]);
+  const [projectImage, setprojectImage] = useState();
+  const [imageId, setImageId] = useState();
 
   useEffect(() => {
+    console.log(projects);
     axios
       .get("https://personal-website--backend.herokuapp.com/project")
       .then(({ data }) => {
@@ -12,7 +18,7 @@ export const Project = () => {
       });
   }, []);
 
-  const readURL = input => {
+  /* const readURL = input => {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
 
@@ -26,11 +32,77 @@ export const Project = () => {
 
   $("#imgInp").change(function() {
     readURL(this);
-  });
+  }); */
 
-  const addProject = () => {};
+  const addProject = () => {
+    axios
+      .post("https://personal-website--backend.herokuapp.com/project", {
+        projectName,
+        subtitle,
+        technologies,
+        projectImage
+      })
+      .then(newProject => {
+        const newProjectsArray = [...projects, newProject.data.data];
+        setProjects(newProjectsArray);
+      });
+  };
 
   const deleteProject = id => {};
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addProject();
+  };
+
+  const onAdd = () => {
+    const newArray = [...technologies, " "];
+    setTechnologies(newArray);
+  };
+
+  const onDelete = index => {
+    console.log(index)
+    const newArray = [
+      ...technologies.slice(0, index),
+      ...technologies.slice(index + 1)
+    ];
+    setTechnologies(newArray);
+  };
+
+  const handleChange = (index, value) => {
+    const newArray = technologies.slice(0);
+    newArray[index] = value;
+    setTechnologies(newArray);
+  };
+
+
+  const uploadImage = (e) => {
+    // let imageObj = {};
+
+      let imageFormObj = new FormData();
+
+      imageFormObj.append("imageName", "multer-image-" + Date.now());
+      imageFormObj.append("imageData", e.target.files[0]);
+
+      setprojectImage(URL.createObjectURL(e.target.files[0]))
+
+      axios.post(`https://personal-website--backend.herokuapp.com/image/uploadmulter`, imageFormObj)
+        .then(({data}) => {
+          if (data.success) {
+            setImageId(data.data.imageData);
+            setprojectImage(`https://personal-website--backend.herokuapp.com/${data.data.imageData}`)
+            alert("Image has been successfully uploaded using multer");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          alert("Error while uploading image using multer");
+        });
+  }
+
+
+
+
 
   return (
     <>
@@ -39,32 +111,66 @@ export const Project = () => {
         <div>Subtitles</div>
         <div>Technologies</div>
         <div>Project Image</div>
-        {projects.length ? (
+        {projects ? (
           projects.map(project => {
-            return <div>{project.projectName} : {project.subtitle}</div>;
+            return (
+              <div key={project._id}>
+                {project.projectName} : {project.subtitle}
+                {project.technologies
+                  ? project.technologies.map((technology, index) => {
+                      return <span key={index}>{technology}</span>;
+                    })
+                  : null}
+                {project.projectImage ? <img src={project.projectImage} height="200px" width="200px"/> : null}
+              </div>
+            );
           })
         ) : (
           <div>None</div>
         )}
       </div>
-      <form
-        action="https://personal-website--backend.herokuapp.com/project"
-        method="POST"
-      >
+      <form onSubmit={handleSubmit}>
         <div style={styles.form}>
-          Project Name: <input name="projectName" required />
-          Subititles: <input name="subtitle" required />
-          Technologies: <input />
+          Project Name:
+          <input
+            name="projectName"
+            value={projectName}
+            onChange={e => setProjectName(e.target.value)}
+            required
+          />
+          Subititles:
+          <input
+            name="subtitle"
+            value={subtitle}
+            onChange={e => setSubtitle(e.target.value)}
+            required
+          />
+          Technologies:
+          {technologies.map((technology, index) => {
+            return (
+              <div key={index}>
+                <input
+                  name={technology}
+                  value={technology}
+                  onChange={e => handleChange(index, e.target.value)}
+                />
+                <span onClick={() => onDelete(index)}>x</span>
+              </div>
+            );
+          })}
+          <button type="button" onClick={() => onAdd()}>
+            Add
+          </button>
           Project Image :
-          <input type="file" id="imgInp" required />
+          <input type="file" id="imgInp" required onChange={e => uploadImage(e)}/>
           <img
             id="blah"
-            src="#"
+            src={projectImage}
             alt="No image selected"
             height="300px"
             width="300px"
           />
-          <button tyoe="submit">Submit</button>
+          <button type="submit">Submit</button>
         </div>
       </form>
     </>
--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -1,11 +1,63 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import {Project} from "./Project.js";
+import { Project } from "./Project.js";
 
 const Admin = () => {
   const [data, setData] = useState([]);
-  return <div>Admin<Project /></div>;
+  return (
+    <div>
+      {/* <header id="top-bar" className="navbar-fixed-top animated-header">
+        <div className="container">
+          <div className="navbar-header">
+            <button
+              type="button"
+              className="navbar-toggle"
+              data-toggle="collapse"
+              data-target=".navbar-collapse"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <div className="navbar-brand">
+              <a href="index.html">
+                ADMIN
+              </a>
+            </div>
+          </div>
+          <nav
+            className="collapse navbar-collapse navbar-right"
+            role="navigation"
+          >
+            <div className="main-menu">
+              <ul className="nav navbar-nav navbar-right">
+                <li>
+                  <a onClick={() => props.setPage("Home")}>Home</a>
+                </li>
+                <li>
+                  <a onClick={() => props.setPage("Education")}>Eduaction</a>
+                </li>
+                <li>
+                  <a onClick={() => props.setPage("Experience")}>
+                    Work Experience
+                  </a>
+                </li>
+                <li>
+                  <a onClick={() => props.setPage("Projects")}>Projects</a>
+                </li>
+                <li>
+                  <a onClick={() => props.setPage("Contact")}>Contact</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header> */}
+      <Project />
+    </div>
+  );
 };
 
 export default Admin;
