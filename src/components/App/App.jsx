2596ddc53a78b20655efc08b8baaef9ee6dfa362
Rakesh Jain <rakeshvora2007@gmail.com>
2020-05-13 21:42:48 -0400

    generate token phase #1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,98 @@
-import React, {useState} from 'react';
-import Navbar from '../Navbar/Navbar.jsx';
-import DisplayPage from '../DisplayPage/DisplayPage.jsx';
-import Footer from '../Footer/Footer.jsx'
+import React, { useState, useEffect } from "react";
+import Navbar from "../Navbar/Navbar.jsx";
+import DisplayPage from "../DisplayPage/DisplayPage.jsx";
+import Footer from "../Footer/Footer.jsx";
+import axios from "axios";
 
 const App = () => {
-    const [page, setPage] = useState();
-    return (
-        <div style={{position: "relative"}}>
-           <Navbar setPage={setPage}/>
-           <DisplayPage page={page}/>
-           <Footer /> 
-        </div>
-    )
-}
+  const [page, setPage] = useState();
 
-export default App
+  useEffect(() => {
+    if (window) {
+      window.addEventListener("keydown", generateToken);
+    }
+    return () => {
+      window.removeEventListener("keydown", generateToken);
+    };
+  }, []);
+
+  const generateToken = async event => {
+    if (event.key === "%") {
+      window.sessionStorage.setItem("generatorTokenInput", "%");
+    }
+    if (event.key === "g") {
+      if (window.sessionStorage.getItem("generatorTokenInput")) {
+        let val = window.sessionStorage.getItem("generatorTokenInput");
+        window.sessionStorage.setItem("generatorTokenInput", val + "g");
+      }
+    }
+
+    if (event.key === "e") {
+      if (window.sessionStorage.getItem("generatorTokenInput")) {
+        let val = window.sessionStorage.getItem("generatorTokenInput");
+        window.sessionStorage.setItem("generatorTokenInput", val + "e");
+      }
+    }
+
+    if (event.key === "n") {
+      if (window.sessionStorage.getItem("generatorTokenInput")) {
+        let val = window.sessionStorage.getItem("generatorTokenInput");
+        window.sessionStorage.setItem("generatorTokenInput", val + "n");
+      }
+    }
+
+    if (event.key === "r") {
+      if (window.sessionStorage.getItem("generatorTokenInput")) {
+        let val = window.sessionStorage.getItem("generatorTokenInput");
+        window.sessionStorage.setItem("generatorTokenInput", val + "r");
+      }
+    }
+
+    if (event.key === "a") {
+      if (window.sessionStorage.getItem("generatorTokenInput")) {
+        let val = window.sessionStorage.getItem("generatorTokenInput");
+        window.sessionStorage.setItem("generatorTokenInput", val + "a");
+      }
+    }
+
+    if (event.key === "t") {
+      if (window.sessionStorage.getItem("generatorTokenInput")) {
+        let val = window.sessionStorage.getItem("generatorTokenInput");
+        window.sessionStorage.setItem("generatorTokenInput", val + "t");
+      }
+    }
+    if (event.key === "o") {
+      if (window.sessionStorage.getItem("generatorTokenInput")) {
+        let val = window.sessionStorage.getItem("generatorTokenInput");
+        window.sessionStorage.setItem("generatorTokenInput", val + "o");
+      }
+    }
+
+    let regex = /%generator/;
+    let value = window.sessionStorage.getItem("generatorTokenInput");
+    if (value && value.match(regex)) {
+      let key = prompt("Please enter key");
+      window.sessionStorage.removeItem("generatorTokenInput");
+      if (key) {
+        let response = await axios.post(
+          "https://personal-website--backend.herokuapp.com/generateToken",
+          { body: { key } }
+        );
+        console.log(response);
+        alert("Generated Token: " + response.data);
+      } else {
+        alert("wrong generator");
+      }
+    }
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <Navbar setPage={setPage} />
+      <DisplayPage page={page} />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
