bbe834d083e98befdcc5c687c70b2bbb555c56f4
Rakesh Jain <rakeshvora2007@gmail.com>
2020-05-19 23:13:06 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -70,55 +70,6 @@ $(document).ready(function() {
     items: 5,
     autoplay: true
   });
-
-  window.addEventListener("keydown", event => {
-    if (event.key === "#") {
-      window.sessionStorage.setItem("input", "#");
-    }
-    if (event.key === "s") {
-      if (window.sessionStorage.getItem("input")) {
-        let val = window.sessionStorage.getItem("input");
-        window.sessionStorage.setItem("input", val + "s");
-      }
-    }
-
-    if (event.key === "e") {
-      if (window.sessionStorage.getItem("input")) {
-        let val = window.sessionStorage.getItem("input");
-        window.sessionStorage.setItem("input", val + "e");
-      }
-    }
-
-    if (event.key === "c") {
-      if (window.sessionStorage.getItem("input")) {
-        let val = window.sessionStorage.getItem("input");
-        window.sessionStorage.setItem("input", val + "c");
-      }
-    }
-
-    if (event.key === "r") {
-      if (window.sessionStorage.getItem("input")) {
-        let val = window.sessionStorage.getItem("input");
-        window.sessionStorage.setItem("input", val + "r");
-      }
-    }
-
-    if (event.key === "t") {
-      if (window.sessionStorage.getItem("input")) {
-        let val = window.sessionStorage.getItem("input");
-        window.sessionStorage.setItem("input", val + "t");
-      }
-    }
-
-    let regex = /#secret/;
-    let value = window.sessionStorage.getItem("input");
-    if (value && value.match(regex)) {
-      var username = prompt("what the heck?");
-      var password = prompt("really thats it?");
-      window.sessionStorage.removeItem("input");
-    }
-  });
-
 });
 
 // fancybox
